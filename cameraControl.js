0d74ecfc238c1ed332e0c948ddca56a244a126f1
hi-go1201 <60642007+hi-go1201@users.noreply.github.com>
2020-10-05 21:40:42 +0900

    Update cameraControl.js

--- cameraControl.js
+++ cameraControl.js
@@ -356,7 +356,7 @@ function addWebGL() {
         
         // ToDo:スマホのセンサ情報用いてスマホの傾きに応じて3Dモデルの奥行きの角度調整
         
-        model1.visible = true;
+        //model1.visible = true;
       } else if(detectWatchArea_flag == false){
         model1.visible = false;
       }
@@ -376,7 +376,7 @@ function addWebGL() {
         
         // ToDo:スマホのセンサ情報用いてスマホの傾きに応じて3Dモデルの奥行きの角度調整
         
-        //model2.visible = true;
+        model2.visible = true;
       } else if(detectRingArea_flag == false){
         model2.visible = false;
       }

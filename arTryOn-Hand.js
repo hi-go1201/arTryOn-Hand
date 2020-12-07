0c95f240072ad3fa523339b571013e81bbb7f4d9
hi-go1201 <60642007+hi-go1201@users.noreply.github.com>
2020-12-08 02:53:54 +0900

    Update arTryOn-Hand.js

--- arTryOn-Hand.js
+++ arTryOn-Hand.js
@@ -562,7 +562,7 @@ function processARTryOn() {
                 //上向きベクトルを生成
                 var axis = new THREE.Vector3(); //←---------------------------------（１）
                 var theta = THREE.Math.degToRad(-90 * alpha);　//正面向くように−90固定?
-                var phi = THREE.Math.degToRad(model_info.angle); //手首の角度
+                var phi = THREE.Math.degToRad(model_info.angle * alpha); //手首の角度
                 var angle = THREE.Math.degToRad(fixAngle); //手首の回転
                 axis.z = Math.cos(theta);
                 axis.x = Math.sin(theta) * Math.cos(phi);
